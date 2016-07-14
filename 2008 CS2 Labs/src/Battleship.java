088ebc361f6686347e62255dfad006aa377b5e93
Nick Tollefson <nmt3242@rit.edu>
2016-07-14 17:01:02 -0400

    Added more classes

--- 2008 CS2 Labs/src/Battleship.java
+++ 2008 CS2 Labs/src/Battleship.java
@@ -9,27 +9,48 @@ public class Battleship {
         Ship my = new Ship("DHHH");
         System.out.println(my.direction);
         System.out.println(my.size);
+        System.out.println(my.x);
+        System.out.println(my.y);
+
+        Ship my2 = new Ship("CBFB");
+        System.out.println(my2.direction);
+        System.out.println(my2.size);
+
+        Ship my3 = new Ship("cdcf");
+        System.out.println(my3.direction);
+        System.out.println(my3.size);
     }
 }
 
 class Tile {
     private String tile = "░ ";
+    private char ship = '!';
 
-    Tile() {
-    }
-
-    void set_tile() {
-        tile = "winner";
-    }
+    Tile() {}
 
     void get_tile() {
         System.out.print(tile);
+    }
+
+    void ship_tile (char letter) {
+        ship = letter;
+    }
+
+    char fire_tile() {
+        char status = 'g';
+
+        if (ship == '!') {
+
+        }
+        return status;
     }
 }
 
 class Ship{
     static int letter = 0;
     char ship_letter;
+    int x;
+    int y;
     String direction;
     int size;
     int hits = 0;
@@ -38,20 +59,26 @@ class Ship{
     Ship(String cords){
         letter++;
         ship_letter = (char)(letter +'A' - 1);
-        String[] coordinates = cords.split("(?!^)");
-        if(coordinates[0] == coordinates[2]) {
-            direction = "vertical";
-            size = (Integer.valueOf(coordinates[2]) - Integer.valueOf(coordinates[0]));
+        y = (int)cords.charAt(0) - 65;
+        x = cords.charAt(1) - 65;
+
+        if(cords.charAt(0) == cords.charAt(2)) {
+            direction = "horizontal";
+            size = (cords.charAt(3) - cords.charAt(1)) + 1;
         }
         else {
-            direction = "horizontal";
-            size = (Integer.valueOf(coordinates[3]) - Integer.valueOf(coordinates[1]));
+            direction = "vertical";
+            size = (cords.charAt(2) - cords.charAt(0)) + 1;
         }
     }
 
-    Grid place(Grid grid) {
-
-        return grid;
+    Grid place(Grid grid[][]) {
+        if (direction.equals("horizontal")) {
+            for (int i = 0; i < size; i++) {
+                grid[x + i][y] = grid[x + i][y].ship_tile(ship_letter);
+            }
+        }
+        return grid[][];
     }
 }
 
